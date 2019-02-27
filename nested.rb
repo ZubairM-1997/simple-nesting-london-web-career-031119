63f3a8ccb306215ad0a186ebf6a961b6fe7015df
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-27 03:40:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,12 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
+
 end
+
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -80,8 +85,12 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {:known_for =>"Ruby", :languages => ["LISP", "C"]}    
 end
+
+
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -101,8 +110,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
@@ -121,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie] = {:age => "27"}
 
 end
